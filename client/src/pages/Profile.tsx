9fea31d1ef097c758cb6be89d21d9641bd9c6872
omotayo olanrewaju <omotayo.olanrewaju@nimble-byte.com>
2026-05-05 11:48:23 +0100

    lightbox added

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -2,15 +2,20 @@ import { useEffect, useState } from "react";
 import { getUser } from "../api/api";
 import { useParams } from "react-router-dom";
 import { useAuth } from "../auth/AuthContext";
+import Lightbox from "../components/Lightbox";
+import type { User } from "../types/users";
 
 export default function Profile() {
   const { id } = useParams();
   const { isLoggedIn } = useAuth();
 
-  const [user, setUser] = useState<any>(null);
+  const [user, setUser] = useState<User | null>(null);
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
 
   useEffect(() => {
-    getUser(id!, isLoggedIn).then(setUser);
+    if (!id) return;
+
+    getUser(id, isLoggedIn).then(setUser);
   }, [id, isLoggedIn]);
 
   if (!user) return <p>Loading...</p>;
@@ -20,10 +25,23 @@ export default function Profile() {
       <h2>{user.name}</h2>
 
       <div className="grid grid-cols-3 gap-2 mt-4">
-        {user.images.map((img: any) => (
-          <img key={img.id} src={img.url} />
+        {user.images.map((img, i) => (
+          <img
+            key={img.id}
+            src={img.url}
+            className="cursor-pointer hover:opacity-80"
+            onClick={() => setSelectedIndex(i)}
+          />
         ))}
       </div>
+
+      {selectedIndex !== null && (
+        <Lightbox
+          images={user.images}
+          startIndex={selectedIndex}
+          onClose={() => setSelectedIndex(null)}
+        />
+      )}
     </div>
   );
 }
