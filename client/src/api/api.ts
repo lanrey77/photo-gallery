3b56eb393bde3b3e96de1056682c6786a40ade74
omotayo olanrewaju <omotayo.olanrewaju@nimble-byte.com>
2026-05-05 11:47:48 +0100

    updated

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,17 +1,23 @@
 const BASE = "http://localhost:5000";
 
+import type { User } from "../types/users";
+
 export const getUsers = async (location?: string) => {
   const res = await fetch(
-    `${BASE}/users${location ? `?location=${location}` : ""}`
+    `${BASE}/users${location ? `?location=${location}` : ""}`,
   );
   return res.json();
 };
 
-export const getUser = async (id: string, loggedIn: boolean) => {
+export const getUser = async (
+  id: string,
+  isLoggedIn: boolean,
+): Promise<User> => {
   const res = await fetch(`${BASE}/users/${id}`, {
     headers: {
-      "x-user": loggedIn ? "true" : ""
-    }
+      "x-user": isLoggedIn ? "true" : "",
+    },
   });
+
   return res.json();
 };
