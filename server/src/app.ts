a408227aff0381e4ca2dd7465189ff1baa4cef03
omotayo olanrewaju <omotayo.olanrewaju@nimble-byte.com>
2026-05-05 10:29:56 +0100

    backend intial setup

---
+++ server/src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import userRoutes from "../routes/user.route.ts"
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/users", userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
