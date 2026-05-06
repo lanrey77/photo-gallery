ea55b4899ea63aa5bd82689b5ff376f3909212b9
lanrey77 <lanrey77@gmail.com>
2026-05-06 10:01:53 +0100

    refactored

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,23 +1,18 @@
 import { Request, Response } from "express";
-import * as userService from "../services/user.service";
-
-
+import * as userService from "../services/user.service.js";
 
 export const getUsers = (req: Request, res: Response): void => {
   const { location } = req.query;
 
-  const users = userService.getAllUsers(
-    location as string | undefined
-  );
+  const users = userService.getAllUsers(location as string | undefined);
 
   res.json(users);
 };
 
 export const getUser = (req: Request, res: Response): void => {
-  const user = userService.getUserById(
-    req.params.id,
-    (req as any).isLoggedIn
-  );
+  const id = Array.isArray(req.params.id) ? req.params.id[0] : req.params.id;
+
+  const user = userService.getUserById(id, (req as any).isLoggedIn);
 
   if (!user) {
     res.status(404).json({ message: "Not found" });
